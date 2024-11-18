d21e584260c8b7913ed2aa3b2b99b0f741358352
Matthew Raison <matthewraison@outlook.com>
2024-11-17 20:17:04 -0500

    cleanup

--- notifyweb/notify/views/base.py
+++ notifyweb/notify/views/base.py
@@ -1,4 +1,5 @@
 # Create your views here.
+import os
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
@@ -10,6 +11,8 @@ from ..serializers.device import DeviceSerializer
 from ..models import Device
 
 logger = logging.getLogger("dummy")
+
+node_manager_url = os.environ.get('NODE_MANAGER_URL', 'http://127.0.0.1:8888/config')
 
 def SystemConfig(request):
     queryset = Device.objects.all()
@@ -27,7 +30,7 @@ def SystemConfig(request):
     if request.method == 'POST':
         node_count = request.POST.get('node_count', 1)
         requests.post(
-            'http://127.0.0.1:8888/config',
+            node_manager_url,
             json={
                 'node_count': int(node_count)
             }
